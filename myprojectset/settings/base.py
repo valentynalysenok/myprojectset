dd3a7db8ac1c2cdd5db47edbf47374c3a58d620a
valentynalysenok <valentynalysenok@planeks.net>
2020-07-06 17:26:56 +0300

    added social_django to installed apps

--- myprojectset/settings/base.py
+++ myprojectset/settings/base.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'social_django',
     'django_extensions',
 
     # 'mptt',
