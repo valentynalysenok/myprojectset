533606312a745775e3eac430fdfb32ce12dcd979
valentynalysenok <valentynalysenok@planeks.net>
2020-07-06 17:06:20 +0300

    added comment

--- myprojectset/wsgi.py
+++ myprojectset/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myprojectset.production')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myprojectset.production')
 
 application = get_wsgi_application()
